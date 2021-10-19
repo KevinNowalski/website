d8f119288042f935b9f30b971ab26d5026de75dc
Kevin Nowalski <kevinnowalskisite@gmail.com>
2021-10-19 01:29:00 -0400

    Styling update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,11 +10,15 @@
 if(typeof $.fn.owlCarousel !== "undefined"){
 	var portfolioLinksCount = $('.portfolio-link').length;
 	$("#portfolio-links").owlCarousel({
-		items: (portfolioLinksCount >= 2 ? 2 : portfolioLinksCount),
+		items: (portfolioLinksCount >= 3 ? 3 : portfolioLinksCount),
 		margin: 30,
-		nav: portfolioLinksCount > 2 ? true : false,
+		nav: portfolioLinksCount > 3 ? true : false,
 		navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+		loop: true,
 		responsive:{
+			991:{
+				items: (portfolioLinksCount >= 3 ? 3 : portfolioLinksCount)
+			},
 			767:{
 				items: (portfolioLinksCount >= 2 ? 2 : portfolioLinksCount)
 			},
